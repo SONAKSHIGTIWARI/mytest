eb9f7405b555a81d2c14773ebfd14fcd1dd624a9
[Sonakshi Tiwari] <[sonakshitiwari460@gmail.com]>
2025-01-15 19:38:21 +0530

    task on js array of object

--- script.js
+++ script.js
@@ -80,18 +80,52 @@
 //}
 //withAsyncAwait()
 
-async function withAsyncAwait() {
-    try{
-        let promise = new Promise((resolve, reject) => {
-            reject("Hello World")
-        });
-        await promise;
-    }catch{
-        console.log("In the Error Block", error);
-    }
+// daut
+//async function withAsyncAwait() {
+//    try{
+//        let promise = new Promise((resolve, reject) => {
+//            reject("Hello World")
+//        });
+//        await promise;
+//    }catch{
+//        console.log("In the Error Block", error);
+//    }
+//    
+//}
+//withAsyncAwait();
+
+let students  = [
+    { roll_no: 1,name: 'student1', subject: 'Physics' },
+    {  roll_no: 2,name: 'student2', subject: 'Mathematics' },
+    {  roll_no: 3,name: 'student3',subject: 'Chemistry' }
+];
+function createTable(students) {
+    let table = document.getElementById('data-table');
+    let thead = table.querySelector('thead tr');
+    let tbody = table.querySelector('tbody');
     
+
+    let headers = Object.keys(students[0]);
+    headers.forEach(header => {
+        const th = document.createElement('th');
+        th.textContent = header.charAt(0).toUpperCase() + header.slice(1); // Capitalize first letter
+        thead.appendChild(th);
+    });
+
+    
+    students.forEach(item => {
+        let tr = document.createElement('tr');
+        headers.forEach(header => {
+            let td = document.createElement('td');
+            td.textContent = item[header];
+            tr.appendChild(td);
+        });
+        tbody.appendChild(tr);
+    });
 }
-withAsyncAwait()
+
+
+createTable(students);
 
 
 
